055f2af57369f1e88f242ec80377354b009a2dd9
Fabiane Malaquias <malaquiasfr@gmail.com>
2022-07-24 11:33:22 -0300

    Req. 02 - Exibindo as estatísticas musicais

--- desafio2.sql
+++ desafio2.sql
@@ -1,9 +1,9 @@
 SELECT 
   COUNT(ca.songs_id) AS 'cancoes', 
-  COUNT(DISTINCT(ar.artist_id)) AS 'artistas', 
+  COUNT(DISTINCT(ar.artist_id)) AS 'artistas',
   COUNT(DISTINCT(al.albums_id)) AS 'albuns'
 FROM SpotifyClone.songs AS ca
 INNER JOIN SpotifyClone.artists AS ar
-ON ar.artist_id = al.artist_id
 INNER JOIN SpotifyClone.albums AS al
-ON ca.albums_id = al.albums_id;
+ON ca.albums_id = al.albums_id
+AND ar.artist_id = al.artist_id ;
